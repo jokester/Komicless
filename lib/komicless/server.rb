444b4a6eff55fd229689b5413cc29880ce01ec0e
Wang Guan <momocraft@gmail.com>
2013-12-09 23:31:16 +0900

    display index

--- lib/komicless/server.rb
+++ lib/komicless/server.rb
@@ -1,8 +1,28 @@
 require 'rack'
+require 'haml'
 
 module Komicless
   INDEX_HAML = open( File.expand_path "index.haml", File.dirname(__FILE__) ).read.freeze
+  INDEX = ::Haml::Engine.new INDEX_HAML
+
   class Server < ::Rack::Directory
+
+    def list_directory
+      ret = super
+      # @files = [ url, basename, size, type, mtime ]
+      @files.shift
+      ret
+    end
+
+    def filesize_format(int)
+      int
+    end
+
+    def each
+      page = INDEX.render(scope=Object.new, locals={ :files => @files } )
+      page.each_line{|l| yield l }
+    end
+
   end
 
 end

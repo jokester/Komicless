c3f9182342d7c160837237faf32c65af9c1a73b4
Wang Guan <momocraft@gmail.com>
2013-12-09 23:57:29 +0900

    seems to be working

--- lib/komicless/server.rb
+++ lib/komicless/server.rb
@@ -2,11 +2,13 @@ require 'rack'
 require 'haml'
 
 module Komicless
+
   INDEX_HAML = open( File.expand_path "index.haml", File.dirname(__FILE__) ).read.freeze
-  INDEX = ::Haml::Engine.new INDEX_HAML
+  INDEX = ::Haml::Engine.new INDEX_HAML, { :attr_wrapper => '"' } # or there will be ' in filenames
 
   class Server < ::Rack::Directory
 
+    # remove first entry
     def list_directory
       ret = super
       # @files = [ url, basename, size, type, mtime ]
@@ -14,10 +16,12 @@ module Komicless
       ret
     end
 
+    # display filesize in integer
     def filesize_format(int)
       int
     end
 
+    # render with our HAML template
     def each
       page = INDEX.render(scope=Object.new, locals={ :files => @files } )
       page.each_line{|l| yield l }
